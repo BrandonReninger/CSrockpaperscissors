a6de644c0a9d6130fd065c22e4561f905f8e353c
Brandon Reninger <brandonreninger@yahoo.com>
2020-05-11 13:34:50 -0600

    slightly after init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSrockpaperscissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            Console.WriteLine("Wanna play rock, paper, scissors? Y/N");
+            string userInput = Console.ReadLine();
+            bool play = userInput.ToLower() == "y";
+        }
+    }
+}
