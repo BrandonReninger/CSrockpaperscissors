4651f54e3f70fbb067fcf61c2c53226e6f900c08
Brandon Reninger <brandonreninger@yahoo.com>
2020-05-11 15:45:48 -0600

    appears to be working

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace CSrockpaperscissors
             // string[] turns = { "rock", "paper", "scissors" };
             Random random = new Random();
             int computerTurn = random.Next(1, 3);
+            int numberGames = 3;
 
 
             while (play)
@@ -25,12 +26,6 @@ namespace CSrockpaperscissors
                 if (userTurn == "rock" && computerTurn == 1)
                 {
                     System.Console.WriteLine("Nobody won, play again? Y/N");
-                    string choice = Console.ReadLine();
-                    if (choice.ToLower() == "y")
-                    {
-
-                    }
-
                 }
                 else if (userTurn == "rock" && computerTurn == 2)
                 {
@@ -40,12 +35,38 @@ namespace CSrockpaperscissors
                 {
                     System.Console.WriteLine("You Won!");
                 }
+                else if (userTurn == "paper" && computerTurn == 1)
+                {
+                    System.Console.WriteLine("You won!");
+                }
+                else if (userTurn == "paper" && computerTurn == 2)
+                {
+                    System.Console.WriteLine("Nobody Won!");
+                }
+                else if (userTurn == "paper" && computerTurn == 3)
+                {
+                    System.Console.WriteLine("You Lost");
+                }
+                else if (userTurn == "scissors" && computerTurn == 1)
+                {
+                    System.Console.WriteLine("You Lost");
+                }
+                else if (userTurn == "scissors" && computerTurn == 2)
+                {
+                    System.Console.WriteLine("You Lost");
+                }
+                else if (userTurn == "scissors" && computerTurn == 3)
+                {
+                    System.Console.WriteLine("Nobody Won!");
+                }
                 else
                 {
                     play = false;
                 }
+                numberGames--;
 
             }
+            System.Console.WriteLine("Thanks for Playing");
         }
     }
 }
