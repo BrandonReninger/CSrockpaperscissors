372abb925b4095528216de7797221ec03e4e65b6
Brandon Reninger <brandonreninger@yahoo.com>
2020-05-11 16:21:00 -0600

    starting refactoring

--- Program.cs
+++ Program.cs
@@ -14,18 +14,50 @@ namespace CSrockpaperscissors
 
             // string[] turns = { "rock", "paper", "scissors" };
             Random random = new Random();
-            int computerTurn = random.Next(1, 3);
             int numberGames = 3;
+            int computerTurn = random.Next(1, 3);
+            int userTurn = 0;
+            if (userInput == "rock")
+            {
+                userTurn = 1;
+            }
+            else if (userInput == "paper")
+            {
+                userTurn = 2;
+            }
+            else if (userInput == "scissors")
+            {
+                userTurn = 3;
+            }
+            else
+            {
+                Console.WriteLine("Enter either rock, paper, or scissors");
+            }
 
 
             while (play)
             {
+                if (numberGames < 1)
+                {
+                    System.Console.WriteLine("Play again Y/N");
+                    string choice = Console.ReadLine();
+                    if (choice.ToLower() == "y")
+                    {
+                        numberGames = 3;
+                    }
+                    else
+                    {
+                        play = false;
+                    }
+                    continue;
+                }
+
                 System.Console.WriteLine("choose: rock, paper, or scissors");
                 string userTurn = Console.ReadLine();
 
-                if (userTurn == "rock" && computerTurn == 1)
+                if (userTurn == computerTurn)
                 {
-                    System.Console.WriteLine("Nobody won, play again? Y/N");
+                    System.Console.WriteLine("Nobody Won!");
                 }
                 else if (userTurn == "rock" && computerTurn == 2)
                 {
@@ -39,10 +71,6 @@ namespace CSrockpaperscissors
                 {
                     System.Console.WriteLine("You won!");
                 }
-                else if (userTurn == "paper" && computerTurn == 2)
-                {
-                    System.Console.WriteLine("Nobody Won!");
-                }
                 else if (userTurn == "paper" && computerTurn == 3)
                 {
                     System.Console.WriteLine("You Lost");
@@ -54,10 +82,6 @@ namespace CSrockpaperscissors
                 else if (userTurn == "scissors" && computerTurn == 2)
                 {
                     System.Console.WriteLine("You Lost");
-                }
-                else if (userTurn == "scissors" && computerTurn == 3)
-                {
-                    System.Console.WriteLine("Nobody Won!");
                 }
                 else
                 {
